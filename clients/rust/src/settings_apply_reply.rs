abfb9c9609ecba45a52e38588deb178e29579580
Leigh Johnson <leigh@bitsy.ai>
2022-12-01 16:07:55 -0800

    fix settings apply/revert replies

--- clients/rust/src/settings_apply_reply.rs
+++ clients/rust/src/settings_apply_reply.rs
@@ -1,10 +1,10 @@
 // SettingsApplyReply represents a SettingsApplyReply model.
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct SettingsApplyReply {
-    #[serde(rename="app", skip_serializing_if = "Option::is_none")]
-    pub app: Option<Box<crate::SettingsApp>>,
-    #[serde(rename="files", skip_serializing_if = "Option::is_none")]
-    pub files: Option<Vec<crate::SettingsFile>>,
+    #[serde(rename="app")]
+    pub app: Box<crate::SettingsApp>,
+    #[serde(rename="files")]
+    pub files: Vec<crate::SettingsFile>,
     #[serde(rename="git_head_commit")]
     pub git_head_commit: String,
     #[serde(rename="git_history")]
@@ -12,9 +12,9 @@ pub struct SettingsApplyReply {
 }
 
 impl SettingsApplyReply {
-    pub fn new(app: Option<crate::SettingsApp>, files: Option<Vec<crate::SettingsFile>>, git_head_commit: String, git_history: Vec<crate::GitCommit>) -> SettingsApplyReply {
+    pub fn new(app: crate::SettingsApp, files: Vec<crate::SettingsFile>, git_head_commit: String, git_history: Vec<crate::GitCommit>) -> SettingsApplyReply {
         SettingsApplyReply {
-            app: app.map(Box::new),
+            app: Box::new(app),
             files,
             git_head_commit,
             git_history,
