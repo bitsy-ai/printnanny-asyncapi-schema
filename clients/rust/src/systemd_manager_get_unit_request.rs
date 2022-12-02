241561946728bbd2bda671aed206d544d272ffcd
Leigh Johnson <leigh@bitsy.ai>
2022-12-02 13:25:54 -0800

    regenerate models

--- clients/rust/src/systemd_manager_get_unit_request.rs
+++ clients/rust/src/systemd_manager_get_unit_request.rs
@@ -1,12 +1,12 @@
 // SystemdManagerGetUnitRequest represents a SystemdManagerGetUnitRequest model.
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct SystemdManagerGetUnitRequest {
-    #[serde(rename="unit_name", skip_serializing_if = "Option::is_none")]
-    pub unit_name: Option<String>,
+    #[serde(rename="unit_name")]
+    pub unit_name: String,
 }
 
 impl SystemdManagerGetUnitRequest {
-    pub fn new(unit_name: Option<String>) -> SystemdManagerGetUnitRequest {
+    pub fn new(unit_name: String) -> SystemdManagerGetUnitRequest {
         SystemdManagerGetUnitRequest {
             unit_name,
         }
