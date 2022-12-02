108b4ef8b2f7266f52b0b9ea9f109f4b766b7d1a
Leigh Johnson <leigh@bitsy.ai>
2022-12-02 12:40:06 -0800

    fix SystemdUnitChange schema

--- clients/rust/src/systemd_unit_change.rs
+++ clients/rust/src/systemd_unit_change.rs
@@ -1,13 +1,20 @@
 // SystemdUnitChange represents a SystemdUnitChange model.
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct SystemdUnitChange {
-    
+    #[serde(rename="change")]
+    pub change: Box<crate::SystemdUnitChangeState>,
+    #[serde(rename="file")]
+    pub file: String,
+    #[serde(rename="destination")]
+    pub destination: String,
 }
 
 impl SystemdUnitChange {
-    pub fn new() -> SystemdUnitChange {
+    pub fn new(change: crate::SystemdUnitChangeState, file: String, destination: String) -> SystemdUnitChange {
         SystemdUnitChange {
-        
+            change: Box::new(change),
+            file,
+            destination,
         }
     }
 }
