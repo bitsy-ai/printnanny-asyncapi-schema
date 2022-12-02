7fbf0414a5f2019d53f8ff70bc48886288117ed4
Leigh Johnson <leigh@bitsy.ai>
2022-12-02 14:45:37 -0800

    check in generated models

--- clients/typescript/src/SystemdUnitFileState.ts
+++ clients/typescript/src/SystemdUnitFileState.ts
@@ -4,6 +4,8 @@ export enum SystemdUnitFileState {
   ENABLED_MINUS_RUNTIME = "enabled-runtime",
   LINKED = "linked",
   LINKED_MINUS_RUNTIME = "linked-runtime",
+  MASKED = "masked",
+  MASKED_MINUS_RUNTIME = "masked-runtime",
   RESERVED_STATIC = "static",
   DISABLED = "disabled",
   INVALID = "invalid",
