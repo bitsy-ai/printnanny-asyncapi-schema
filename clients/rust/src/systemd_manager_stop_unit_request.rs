961e5a432a69b0e6f5fd46f5a2927eea6ae6417b
Leigh Johnson <leigh@bitsy.ai>
2022-12-02 16:27:31 -0800

    Fix unit_name field not being required in misc message models (#27)
    * fix unit_name field not being required
    
    * check in generated models

--- clients/rust/src/systemd_manager_stop_unit_request.rs
+++ clients/rust/src/systemd_manager_stop_unit_request.rs
@@ -1,12 +1,12 @@
 // SystemdManagerStopUnitRequest represents a SystemdManagerStopUnitRequest model.
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct SystemdManagerStopUnitRequest {
-    #[serde(rename="unit_name", skip_serializing_if = "Option::is_none")]
-    pub unit_name: Option<String>,
+    #[serde(rename="unit_name")]
+    pub unit_name: String,
 }
 
 impl SystemdManagerStopUnitRequest {
-    pub fn new(unit_name: Option<String>) -> SystemdManagerStopUnitRequest {
+    pub fn new(unit_name: String) -> SystemdManagerStopUnitRequest {
         SystemdManagerStopUnitRequest {
             unit_name,
         }
