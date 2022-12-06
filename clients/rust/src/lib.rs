c814bb38f596bb1d5183b956329324d84895032c
Leigh Johnson <leigh@bitsy.ai>
2022-12-06 15:43:28 -0800

    Implement SystemdManagerStartUnitError/SystemdManagerStopUnitError (#37)
    * add SystemdManagerStartUnitError SystemdManagerStopUnitError message schemas
    
    * check in generated code

--- clients/rust/src/lib.rs
+++ clients/rust/src/lib.rs
@@ -71,11 +71,17 @@ pub use self::systemd_manager_start_unit_request::*;
 pub mod systemd_manager_start_unit_reply;
 pub use self::systemd_manager_start_unit_reply::*;
 
+pub mod systemd_manager_start_unit_error;
+pub use self::systemd_manager_start_unit_error::*;
+
 pub mod systemd_manager_stop_unit_request;
 pub use self::systemd_manager_stop_unit_request::*;
 
 pub mod systemd_manager_stop_unit_reply;
 pub use self::systemd_manager_stop_unit_reply::*;
+
+pub mod systemd_manager_stop_unit_error;
+pub use self::systemd_manager_stop_unit_error::*;
 
 pub mod device_info_load_reply;
 pub use self::device_info_load_reply::*;
