c64527931777e96bf090a6ddeb4277d2cb214f3a
Leigh Johnson <leigh@bitsy.ai>
2022-12-06 17:22:34 -0800

    Fix unit_file_state being optional (#39)
    * fix unit_file_state rendered as optional
    
    * generate clients

--- clients/rust/src/systemd_manager_get_unit_file_state_reply.rs
+++ clients/rust/src/systemd_manager_get_unit_file_state_reply.rs
@@ -1,14 +1,14 @@
 // SystemdManagerGetUnitFileStateReply represents a SystemdManagerGetUnitFileStateReply model.
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct SystemdManagerGetUnitFileStateReply {
-    #[serde(rename="unit_file_state", skip_serializing_if = "Option::is_none")]
-    pub unit_file_state: Option<Box<crate::SystemdUnitFileState>>,
+    #[serde(rename="unit_file_state")]
+    pub unit_file_state: Box<crate::SystemdUnitFileState>,
 }
 
 impl SystemdManagerGetUnitFileStateReply {
-    pub fn new(unit_file_state: Option<crate::SystemdUnitFileState>) -> SystemdManagerGetUnitFileStateReply {
+    pub fn new(unit_file_state: crate::SystemdUnitFileState) -> SystemdManagerGetUnitFileStateReply {
         SystemdManagerGetUnitFileStateReply {
-            unit_file_state: unit_file_state.map(Box::new),
+            unit_file_state: Box::new(unit_file_state),
         }
     }
 }
--- clients/typescript/src/SystemdManagerGetUnitFileStateReply.ts
+++ clients/typescript/src/SystemdManagerGetUnitFileStateReply.ts
@@ -1,4 +1,4 @@
 import {SystemdUnitFileState} from './SystemdUnitFileState';
 export interface SystemdManagerGetUnitFileStateReply {
-  unit_file_state?: SystemdUnitFileState;
+  unit_file_state: SystemdUnitFileState;
 }
