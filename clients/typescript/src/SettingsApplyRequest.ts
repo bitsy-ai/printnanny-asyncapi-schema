84592a323e60b86f4933960fa62a8ca7b2b6e757
Leigh Johnson <leigh@bitsy.ai>
2022-12-08 09:18:09 -0800

    Load all settings files in one message reply (#40)
    * load all settings files in one call
    
    * generate ts/rust models

--- clients/typescript/src/SettingsApplyRequest.ts
+++ clients/typescript/src/SettingsApplyRequest.ts
@@ -1,7 +1,5 @@
-import {SettingsApp} from './SettingsApp';
 import {SettingsFile} from './SettingsFile';
 export interface SettingsApplyRequest {
-  app: SettingsApp;
   files: SettingsFile[];
   git_head_commit: string;
   git_commit_msg: string;
