5dd4e722ffb17faec033c653893c6eb3d0537294
Leigh Johnson <leigh@bitsy.ai>
2022-12-08 19:38:42 -0800

    Add Tailscale ipv4/ipv6 to DeviceInfo schema (#44)
    * add  DeviceInfoLoadReply.tailscale_address_ipv4  DeviceInfoLoadReply.tailscale_address_ipv6 as optional properties
    
    * check in generated code

--- clients/rust/src/device_info_load_reply.rs
+++ clients/rust/src/device_info_load_reply.rs
@@ -7,14 +7,20 @@ pub struct DeviceInfoLoadReply {
     pub os_release: String,
     #[serde(rename="printnanny_cli_version")]
     pub printnanny_cli_version: String,
+    #[serde(rename="tailscale_address_ipv4", skip_serializing_if = "Option::is_none")]
+    pub tailscale_address_ipv4: Option<String>,
+    #[serde(rename="tailscale_address_ipv6", skip_serializing_if = "Option::is_none")]
+    pub tailscale_address_ipv6: Option<String>,
 }
 
 impl DeviceInfoLoadReply {
-    pub fn new(issue: String, os_release: String, printnanny_cli_version: String) -> DeviceInfoLoadReply {
+    pub fn new(issue: String, os_release: String, printnanny_cli_version: String, tailscale_address_ipv4: Option<String>, tailscale_address_ipv6: Option<String>) -> DeviceInfoLoadReply {
         DeviceInfoLoadReply {
             issue,
             os_release,
             printnanny_cli_version,
+            tailscale_address_ipv4,
+            tailscale_address_ipv6,
         }
     }
 }
