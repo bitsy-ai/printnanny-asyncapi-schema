644409a7888ad32f6905b55c113777a5ce33cbe7
Leigh Johnson <leigh@bitsy.ai>
2022-12-08 19:36:02 -0800

    check in generated code

--- clients/typescript/src/DeviceInfoLoadReply.ts
+++ clients/typescript/src/DeviceInfoLoadReply.ts
@@ -3,4 +3,6 @@ export interface DeviceInfoLoadReply {
   issue: string;
   os_release: string;
   printnanny_cli_version: string;
+  tailscale_address_ipv4?: string;
+  tailscale_address_ipv6?: string;
 }
