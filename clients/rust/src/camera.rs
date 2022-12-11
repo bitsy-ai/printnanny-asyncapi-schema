d85f740f3d5fc5dc0ada64b9ea64f87dae0770e9
Leigh Johnson <leigh@bitsy.ai>
2022-12-11 10:34:58 -0800

    re-generate model code

--- clients/rust/src/camera.rs
+++ clients/rust/src/camera.rs
@@ -3,17 +3,17 @@
 pub struct Camera {
     #[serde(rename="index")]
     pub index: i32,
-    #[serde(rename="name")]
-    pub name: String,
+    #[serde(rename="device_name")]
+    pub device_name: String,
     #[serde(rename="label")]
     pub label: String,
 }
 
 impl Camera {
-    pub fn new(index: i32, name: String, label: String) -> Camera {
+    pub fn new(index: i32, device_name: String, label: String) -> Camera {
         Camera {
             index,
-            name,
+            device_name,
             label,
         }
     }
