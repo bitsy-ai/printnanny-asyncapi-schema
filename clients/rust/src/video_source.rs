9e17fb8c98ffd7a5f606bf0a7ae428047bf8e753
Leigh Johnson <leigh@bitsy.ai>
2022-12-14 12:09:00 -0800

    regenerate rust model

--- clients/rust/src/video_source.rs
+++ clients/rust/src/video_source.rs
@@ -1,9 +1,9 @@
-// VideoSource represents a union of types: Camera0, PlaybackVideo1
+// VideoSource represents a union of types: Camera, PlaybackVideo
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum VideoSource {
-    #[serde(rename="Camera0")]
-    Camera0(crate::Camera),
-    #[serde(rename="PlaybackVideo1")]
-    PlaybackVideo1(crate::PlaybackVideo),
+    #[serde(rename="Camera")]
+    Camera(crate::Camera),
+    #[serde(rename="PlaybackVideo")]
+    PlaybackVideo(crate::PlaybackVideo),
 }
 
