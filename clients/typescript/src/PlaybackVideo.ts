1c836a44ebdddac29edc94270d7e305b5169193b
Leigh Johnson <leigh@bitsy.ai>
2022-12-14 16:48:59 -0800

    Missing playback video fields (#51)
    * fix PlaybackVideo missing cover, name fields
    
    * generate rust/ts models

--- clients/typescript/src/PlaybackVideo.ts
+++ clients/typescript/src/PlaybackVideo.ts
@@ -1,5 +1,7 @@
 import {PlaybackSourceType} from './PlaybackSourceType';
 export interface PlaybackVideo {
+  cover: string;
+  reserved_name: string;
   uri: string;
   src_type: PlaybackSourceType;
 }
