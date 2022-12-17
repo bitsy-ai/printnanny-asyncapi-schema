90e8f67baa92d12706c5f19c591d310014c30183
Leigh Johnson <leigh@bitsy.ai>
2022-12-16 19:36:25 -0800

    use untagged repr

--- clients/rust/src/video_source.rs
+++ clients/rust/src/video_source.rs
@@ -1,6 +1,6 @@
 // VideoSource represents a union of types: Camera, PlaybackVideo
 #[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
-#[serde(tag = "src_type")]
+#[serde(untagged)]
 pub enum VideoSource {
     #[serde(rename="Camera")]
     Camera(crate::Camera),
