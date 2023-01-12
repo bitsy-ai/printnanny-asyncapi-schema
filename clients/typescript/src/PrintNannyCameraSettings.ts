98d5718475cac72263310fe2010e9b01f232fd57
Leigh Johnson <leigh@bitsy.ai>
2023-01-12 10:32:36 -0800

    generate rs/ts clients

--- clients/typescript/src/PrintNannyCameraSettings.ts
+++ clients/typescript/src/PrintNannyCameraSettings.ts
@@ -1,9 +1,11 @@
-import { PrintNannyDetectionSettings } from './PrintNannyDetectionSettings';
-import { HlsSettings } from './HlsSettings';
-import { Camera } from './Camera';
-import { PlaybackVideo } from './PlaybackVideo';
+import {PrintNannyDetectionSettings} from './PrintNannyDetectionSettings';
+import {HlsSettings} from './HlsSettings';
+import {Camera} from './Camera';
+import {PlaybackVideo} from './PlaybackVideo';
 export interface PrintNannyCameraSettings {
   overlay_udp_port: number;
+  record_video: boolean;
+  cloud_backup: boolean;
   preview: boolean;
   video_framerate: number;
   video_udp_port: number;
