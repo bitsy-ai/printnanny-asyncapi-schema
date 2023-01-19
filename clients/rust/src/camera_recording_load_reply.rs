7853f1cc0f9f996ee39192f73734235bbc940149
Leigh Johnson <leigh@bitsy.ai>
2023-01-19 10:55:22 -0800

    Fix camera recordings additional props (#84)
    * remove additionalProperties from CameraRecordingLoadReply schema
    
    * fix: additionalProperties field

--- clients/rust/src/camera_recording_load_reply.rs
+++ clients/rust/src/camera_recording_load_reply.rs
@@ -1,20 +1,17 @@
 // CameraRecordingLoadReply represents a CameraRecordingLoadReply model.
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct CameraRecordingLoadReply {
     #[serde(rename="recordings")]
     pub recordings: Vec<crate::VideoRecording>,
     #[serde(rename="current", skip_serializing_if = "Option::is_none")]
     pub current: Option<Box<crate::VideoRecording>>,
-    #[serde(rename="additionalProperties", skip_serializing_if = "Option::is_none")]
-    pub additional_properties: Option<std::collections::HashMap<String, serde_json::Value>>,
 }
 
 impl CameraRecordingLoadReply {
-    pub fn new(recordings: Vec<crate::VideoRecording>, current: Option<crate::VideoRecording>, additional_properties: Option<std::collections::HashMap<String, serde_json::Value>>) -> CameraRecordingLoadReply {
+    pub fn new(recordings: Vec<crate::VideoRecording>, current: Option<crate::VideoRecording>) -> CameraRecordingLoadReply {
         CameraRecordingLoadReply {
             recordings,
             current: current.map(Box::new),
-            additional_properties,
         }
     }
 }
