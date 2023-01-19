00bb84a70e1b0089025b6e29a2ec4dd0411929e1
Leigh Johnson <leigh@bitsy.ai>
2023-01-19 12:40:04 -0800

    add mp4_download_url

--- clients/typescript/src/VideoRecording.ts
+++ clients/typescript/src/VideoRecording.ts
@@ -5,6 +5,8 @@ export interface VideoRecording {
   recording_start?: string;
   recording_end?: string;
   mp4_file_name: string;
+  mp4_download_url?: string;
+  mp4_upload_url?: string;
   gcode_file_name?: string;
   cloud_sync_status: VideoRecordingStatus;
   cloud_sync_start?: string;
