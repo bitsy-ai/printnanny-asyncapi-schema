ace0c570a6a7c5dafc1ad0631473f7e50f75e811
Leigh Johnson <leigh@bitsy.ai>
2023-01-18 17:21:25 -0800

    Fix start end type (#83)
    * use dateTime format for start/end
    
    * fix: PrintNannyCloudSyncReply
    
    * fix required fields

--- clients/rust/src/print_nanny_cloud_sync_reply.rs
+++ clients/rust/src/print_nanny_cloud_sync_reply.rs
@@ -1,14 +1,14 @@
 // PrintNannyCloudSyncReply represents a PrintNannyCloudSyncReply model.
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct PrintNannyCloudSyncReply {
-    #[serde(rename="start", skip_serializing_if = "Option::is_none")]
-    pub start: Option<i64>,
-    #[serde(rename="end", skip_serializing_if = "Option::is_none")]
-    pub end: Option<i64>,
+    #[serde(rename="start")]
+    pub start: String,
+    #[serde(rename="end")]
+    pub end: String,
 }
 
 impl PrintNannyCloudSyncReply {
-    pub fn new(start: Option<i64>, end: Option<i64>) -> PrintNannyCloudSyncReply {
+    pub fn new(start: String, end: String) -> PrintNannyCloudSyncReply {
         PrintNannyCloudSyncReply {
             start,
             end,
