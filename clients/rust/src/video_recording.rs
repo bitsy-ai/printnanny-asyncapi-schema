0b4cae453006f116868aad1ea4d01df1572a0d7d
Leigh Johnson <leigh@bitsy.ai>
2023-01-19 12:31:26 -0800

    fix casing

--- clients/rust/src/video_recording.rs
+++ clients/rust/src/video_recording.rs
@@ -11,8 +11,8 @@ pub struct VideoRecording {
     pub recording_end: Option<String>,
     #[serde(rename="mp4_file_name")]
     pub mp4_file_name: String,
-    #[serde(rename="gcode_file-name", skip_serializing_if = "Option::is_none")]
-    pub gcode_file_minus_name: Option<String>,
+    #[serde(rename="gcode_file_name", skip_serializing_if = "Option::is_none")]
+    pub gcode_file_name: Option<String>,
     #[serde(rename="cloud_sync_status")]
     pub cloud_sync_status: Box<crate::VideoRecordingStatus>,
     #[serde(rename="cloud_sync_start", skip_serializing_if = "Option::is_none")]
@@ -22,14 +22,14 @@ pub struct VideoRecording {
 }
 
 impl VideoRecording {
-    pub fn new(id: String, recording_status: crate::VideoRecordingStatus, recording_start: Option<String>, recording_end: Option<String>, mp4_file_name: String, gcode_file_minus_name: Option<String>, cloud_sync_status: crate::VideoRecordingStatus, cloud_sync_start: Option<String>, cloud_sync_end: Option<String>) -> VideoRecording {
+    pub fn new(id: String, recording_status: crate::VideoRecordingStatus, recording_start: Option<String>, recording_end: Option<String>, mp4_file_name: String, gcode_file_name: Option<String>, cloud_sync_status: crate::VideoRecordingStatus, cloud_sync_start: Option<String>, cloud_sync_end: Option<String>) -> VideoRecording {
         VideoRecording {
             id,
             recording_status: Box::new(recording_status),
             recording_start,
             recording_end,
             mp4_file_name,
-            gcode_file_minus_name,
+            gcode_file_name,
             cloud_sync_status: Box::new(cloud_sync_status),
             cloud_sync_start,
             cloud_sync_end,
--- clients/typescript/src/VideoRecording.ts
+++ clients/typescript/src/VideoRecording.ts
@@ -5,7 +5,7 @@ export interface VideoRecording {
   recording_start?: string;
   recording_end?: string;
   mp4_file_name: string;
-  gcode_file_minus_name?: string;
+  gcode_file_name?: string;
   cloud_sync_status: VideoRecordingStatus;
   cloud_sync_start?: string;
   cloud_sync_end?: string;
