bf848e3e66910510899630a59be67925c374b67b
Leigh Johnson <leigh@bitsy.ai>
2023-01-19 11:10:34 -0800

    fix: VideoRecording dateTime field types (#85)
    * fix: VideoRecording dateTime field types
    
    * generate rust/ts models

--- clients/typescript/src/VideoRecording.ts
+++ clients/typescript/src/VideoRecording.ts
@@ -2,11 +2,11 @@ import {VideoRecordingStatus} from './VideoRecordingStatus';
 export interface VideoRecording {
   id: string;
   recording_status: VideoRecordingStatus;
-  recording_start?: number;
-  recording_end?: number;
+  recording_start?: string;
+  recording_end?: string;
   recording_file_name: string;
   gcode_file_minus_name?: string;
   cloud_sync_status: VideoRecordingStatus;
-  cloud_sync_start?: number;
-  cloud_sync_end?: number;
+  cloud_sync_start?: string;
+  cloud_sync_end?: string;
 }
