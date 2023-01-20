393efa010e58243631ceebd6a7f3be9b05849ae0
Leigh Johnson <leigh@bitsy.ai>
2023-01-19 19:50:35 -0800

    fix OctoPrintCurrentJob

--- clients/rust/src/octo_print_current_job.rs
+++ clients/rust/src/octo_print_current_job.rs
@@ -1,18 +1,23 @@
 // OctoPrintCurrentJob represents a OctoPrintCurrentJob model.
-#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
 pub struct OctoPrintCurrentJob {
-    #[serde(rename="job")]
+    #[serde(rename = "job")]
     pub job: Box<crate::OctoPrintJob>,
-    #[serde(rename="progress")]
+    #[serde(rename = "progress")]
     pub progress: Box<crate::OctoPrintJobProgress>,
-    #[serde(rename="state")]
+    #[serde(rename = "state")]
     pub state: String,
-    #[serde(rename="error", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "error", skip_serializing_if = "Option::is_none")]
     pub error: Option<String>,
 }
 
 impl OctoPrintCurrentJob {
-    pub fn new(job: crate::OctoPrintJob, progress: crate::OctoPrintJobProgress, state: String, error: Option<String>) -> OctoPrintCurrentJob {
+    pub fn new(
+        job: crate::OctoPrintJob,
+        progress: crate::OctoPrintJobProgress,
+        state: String,
+        error: Option<String>,
+    ) -> OctoPrintCurrentJob {
         OctoPrintCurrentJob {
             job: Box::new(job),
             progress: Box::new(progress),
