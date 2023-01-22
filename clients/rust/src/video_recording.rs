214cd33c3ee97b17c618bdcaa04d7cbe7fdedee7
Leigh Johnson <leigh@bitsy.ai>
2023-01-21 20:18:11 -0800

    VideoRecording.recording status and VideoRecording.cloud_sync_status (#90)
    * allow cloud_sync_status and recording_status to be optional fields
    
    * add rust/ts clients

--- clients/rust/src/video_recording.rs
+++ clients/rust/src/video_recording.rs
@@ -3,8 +3,8 @@
 pub struct VideoRecording {
     #[serde(rename="id")]
     pub id: String,
-    #[serde(rename="recording_status")]
-    pub recording_status: Box<crate::VideoRecordingStatus>,
+    #[serde(rename="recording_status", skip_serializing_if = "Option::is_none")]
+    pub recording_status: Option<Box<crate::VideoRecordingStatus>>,
     #[serde(rename="recording_start", skip_serializing_if = "Option::is_none")]
     pub recording_start: Option<String>,
     #[serde(rename="recording_end", skip_serializing_if = "Option::is_none")]
@@ -17,8 +17,8 @@ pub struct VideoRecording {
     pub mp4_upload_url: Option<String>,
     #[serde(rename="gcode_file_name", skip_serializing_if = "Option::is_none")]
     pub gcode_file_name: Option<String>,
-    #[serde(rename="cloud_sync_status")]
-    pub cloud_sync_status: Box<crate::VideoRecordingStatus>,
+    #[serde(rename="cloud_sync_status", skip_serializing_if = "Option::is_none")]
+    pub cloud_sync_status: Option<Box<crate::VideoRecordingStatus>>,
     #[serde(rename="cloud_sync_start", skip_serializing_if = "Option::is_none")]
     pub cloud_sync_start: Option<String>,
     #[serde(rename="cloud_sync_end", skip_serializing_if = "Option::is_none")]
@@ -26,17 +26,17 @@ pub struct VideoRecording {
 }
 
 impl VideoRecording {
-    pub fn new(id: String, recording_status: crate::VideoRecordingStatus, recording_start: Option<String>, recording_end: Option<String>, mp4_file_name: String, mp4_download_url: Option<String>, mp4_upload_url: Option<String>, gcode_file_name: Option<String>, cloud_sync_status: crate::VideoRecordingStatus, cloud_sync_start: Option<String>, cloud_sync_end: Option<String>) -> VideoRecording {
+    pub fn new(id: String, recording_status: Option<crate::VideoRecordingStatus>, recording_start: Option<String>, recording_end: Option<String>, mp4_file_name: String, mp4_download_url: Option<String>, mp4_upload_url: Option<String>, gcode_file_name: Option<String>, cloud_sync_status: Option<crate::VideoRecordingStatus>, cloud_sync_start: Option<String>, cloud_sync_end: Option<String>) -> VideoRecording {
         VideoRecording {
             id,
-            recording_status: Box::new(recording_status),
+            recording_status: recording_status.map(Box::new),
             recording_start,
             recording_end,
             mp4_file_name,
             mp4_download_url,
             mp4_upload_url,
             gcode_file_name,
-            cloud_sync_status: Box::new(cloud_sync_status),
+            cloud_sync_status: cloud_sync_status.map(Box::new),
             cloud_sync_start,
             cloud_sync_end,
         }
--- clients/typescript/src/VideoRecording.ts
+++ clients/typescript/src/VideoRecording.ts
@@ -1,14 +1,14 @@
 import {VideoRecordingStatus} from './VideoRecordingStatus';
 export interface VideoRecording {
   id: string;
-  recording_status: VideoRecordingStatus;
+  recording_status?: VideoRecordingStatus;
   recording_start?: string;
   recording_end?: string;
   mp4_file_name: string;
   mp4_download_url?: string;
   mp4_upload_url?: string;
   gcode_file_name?: string;
-  cloud_sync_status: VideoRecordingStatus;
+  cloud_sync_status?: VideoRecordingStatus;
   cloud_sync_start?: string;
   cloud_sync_end?: string;
 }
