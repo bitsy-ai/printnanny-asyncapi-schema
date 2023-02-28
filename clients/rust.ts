e2415dcc5bf1c0f71173532a598879397813efdf
Leigh Johnson <leigh@bitsy.ai>
2023-02-28 10:19:16 -0800

    bump version to 0.4.0

--- clients/rust.ts
+++ clients/rust.ts
@@ -26,7 +26,7 @@ export async function generate(): Promise<void> {
     supportFiles: true, // generate Cargo.toml and lib.rs
     package: {
       packageName: 'printnanny-asyncapi-models',
-      packageVersion: '0.3.18',
+      packageVersion: '0.4.0',
       // set authors, homepage, repository, and license
       authors: ['Leigh Johnson <leigh@printnanny.ai>'],
       homepage: 'https://github.com/bitsy-ai/printnanny-asyncapi-schema',
