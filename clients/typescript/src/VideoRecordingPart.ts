abe231d4b4b58cf128fec3977607750a75179b39
Leigh Johnson <leigh@bitsy.ai>
2023-03-15 08:53:39 -0700

    generate rust/ts clients

--- clients/typescript/src/VideoRecordingPart.ts
+++ clients/typescript/src/VideoRecordingPart.ts
@@ -2,12 +2,7 @@
 export interface VideoRecordingPart {
   id: string;
   buffer_index: number;
-  buffer_ts: number;
-  buffer_streamtime: number;
   buffer_runningtime: number;
-  buffer_duration: number;
-  buffer_offset: number;
-  buffer_offset_end: number;
   size: number;
   deleted: boolean;
   sync_start?: string;
