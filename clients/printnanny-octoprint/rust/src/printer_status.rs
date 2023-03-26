3bfe8ae953af49536dacac38a64d772943a7fc3a
Leigh Johnson <leigh@bitsy.ai>
2023-03-26 16:54:28 -0700

    generate rust/ts/py clients

--- clients/printnanny-octoprint/rust/src/printer_status.rs
+++ clients/printnanny-octoprint/rust/src/printer_status.rs
@@ -1,27 +1,37 @@
 // PrinterStatus represents a PrinterStatus model.
 #[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum PrinterStatus {
-    #[serde(rename="OPEN_SERIAL")]
-    OpenSerial,
-    #[serde(rename="CONNECTING")]
-    Connecting,
-    #[serde(rename="OPERATIONAL")]
-    Operational,
-    #[serde(rename="PRINTING")]
-    Printing,
-    #[serde(rename="PAUSED")]
-    Paused,
-    #[serde(rename="CLOSED")]
-    Closed,
-    #[serde(rename="ERROR")]
-    Error,
-    #[serde(rename="UNKNOWN")]
-    Unknown,
     #[serde(rename="CLOSED_WITH_ERROR")]
     ClosedWithError,
+    #[serde(rename="CLOSED")]
+    Closed,
+    #[serde(rename="CONNECTING")]
+    Connecting,
+    #[serde(rename="DETECT_BAUDRATE")]
+    DetectBaudrate,
+    #[serde(rename="DETECT_SERIAL")]
+    DetectSerial,
+    #[serde(rename="ERROR")]
+    Error,
+    #[serde(rename="NONE")]
+    None,
+    #[serde(rename="OFFLINE")]
+    Offline,
+    #[serde(rename="OPEN_SERIAL")]
+    OpenSerial,
+    #[serde(rename="OPERATIONAL")]
+    Operational,
+    #[serde(rename="PAUSED")]
+    Paused,
+    #[serde(rename="PRINTING")]
+    Printing,
+    #[serde(rename="TRANSFERING_FILE")]
+    TransferingFile,
+    #[serde(rename="UNKNOWN")]
+    Unknown,
 }
 impl Default for PrinterStatus {
     fn default() -> PrinterStatus {
-        PrinterStatus::OpenSerial
+        PrinterStatus::ClosedWithError
     }
 }
