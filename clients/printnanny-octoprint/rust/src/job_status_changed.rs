37a38a7df2b278cc21431c9b68cbfe24e7f21239
Leigh Johnson <leigh@bitsy.ai>
2023-03-29 09:58:55 -0700

    fix generated rust client

--- clients/printnanny-octoprint/rust/src/job_status_changed.rs
+++ clients/printnanny-octoprint/rust/src/job_status_changed.rs
@@ -1,13 +1,16 @@
 // JobStatusChanged represents a JobStatusChanged model.
-#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq, PartialOrd, Serialize)]
 pub struct JobStatusChanged {
-    #[serde(rename="status")]
+    #[serde(rename = "job", skip_serializing_if = "Option::is_none")]
+    pub job: Option<Box<crate::Job>>,
+    #[serde(rename = "status")]
     pub status: Box<crate::JobStatus>,
 }
 
 impl JobStatusChanged {
-    pub fn new(status: crate::JobStatus) -> JobStatusChanged {
+    pub fn new(job: Option<crate::Job>, status: crate::JobStatus) -> JobStatusChanged {
         JobStatusChanged {
+            job: job.map(Box::new),
             status: Box::new(status),
         }
     }
--- clients/printnanny-octoprint/typescript/src/JobStatusChanged.ts
+++ clients/printnanny-octoprint/typescript/src/JobStatusChanged.ts
@@ -1,5 +1,7 @@
+import {Job} from './Job';
 import {JobStatus} from './JobStatus';
 interface JobStatusChanged {
+  job?: Job;
   reserved_status: JobStatus;
 }
 export { JobStatusChanged };
